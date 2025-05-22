ec45af2e4155ffb9b3d17df325e36d01298f7e0d
Jon Steinich <jonsteinich@perblue.com>
2025-05-22 10:35:50 -0500

    allow setting the LoggerFactory

--- src/KinesisProducerNet/Logging.cs
+++ src/KinesisProducerNet/Logging.cs
@@ -4,7 +4,7 @@ namespace KinesisProducerNet
 {
     public static class Logging
     {
-        public static ILoggerFactory LoggerFactory { get; } = Microsoft.Extensions.Logging.LoggerFactory.Create(
+        public static ILoggerFactory LoggerFactory { get; set; } = Microsoft.Extensions.Logging.LoggerFactory.Create(
             builder => builder.AddConsole().SetMinimumLevel(LogLevel.Debug));
         public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
     }
